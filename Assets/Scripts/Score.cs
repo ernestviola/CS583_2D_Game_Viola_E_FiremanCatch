6a5744f8311a0e6ec50bcfdc5c928f0a1855eef9
Ernest Viola <ernest.viol@gmail.com>
2017-11-26 15:12:08 -0800

    Created restart button to restart the game

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Score : MonoBehaviour {
+
+	public Text scoreText;
+	public int civilianValue;
+
+	private int score;
+
+	// Use this for initialization
+	void Start () {
+		score = 0;
+		UpdateScore ();
+	}
+
+	void OnTriggerEnter2D () {
+		score += civilianValue;
+		UpdateScore ();
+	}
+
+	void UpdateScore () {
+		scoreText.text = "Score: " + score;
+	}
+}
--- Assets/Scripts/ScriptGameController.cs
+++ Assets/Scripts/ScriptGameController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class ScriptGameController : MonoBehaviour {
@@ -9,6 +10,9 @@ public class ScriptGameController : MonoBehaviour {
 	public Rigidbody2D rb2D;
 	public float timeLeft;
 	private float maxWidth;
+	public Text timerText;
+	public GameObject gameOverText;
+	public GameObject restartButton;
 
 	// Use this for initialization
 	void Start () {
@@ -23,8 +27,15 @@ public class ScriptGameController : MonoBehaviour {
 		StartCoroutine (Spawn ());
 	}
 
+	public void StartGame () {
+
+	}
+
 	void FixedUpdate () {
 		timeLeft -= Time.deltaTime;
+		if (timeLeft < 0)
+			timeLeft = 0;
+		UpdateText ();
 	}
 
 	IEnumerator Spawn () {
@@ -37,7 +48,14 @@ public class ScriptGameController : MonoBehaviour {
             );
 			Quaternion spawnRotation = Quaternion.identity;
 			Instantiate (civilian, spawnPosition, spawnRotation);
-			yield return new WaitForSeconds (Random.Range (1.0f,2.0f));
+			yield return new WaitForSeconds (Random.Range (.5f,.8f));
 		}
+		yield return new WaitForSeconds (2.0f);
+		gameOverText.SetActive (true);
+		restartButton.SetActive (true);
+	}
+
+	void UpdateText () {
+		timerText.text = "Time Left: " + Mathf.RoundToInt(timeLeft);
 	}
 }
